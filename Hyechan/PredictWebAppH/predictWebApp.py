d39da21fc9d6153e044a1348c85fcd3aac4eb643
hyechanj <hj48@students.calvin.edu>
2020-06-02 15:41:07 -0400

    separated into my own folder

---
+++ Hyechan/PredictWebAppH/predictWebApp.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/predict')
+def predict_form():
+    return render_template('predict-form.html')
+
+@app.route('/predict', methods=['POST'])
+def predict_scoring():
+    prompt_text = request.form['text']
+    return prompt_text
