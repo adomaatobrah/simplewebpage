1616d4efaa0292e9ff2756e1521909c0eafe7742
hj48 <hj48@ds1.cs.calvin.edu>
2020-06-02 15:04:03 -0400

    added html file

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/predict')
+def predict_form():
+    return render_template('predict-form.html')
+
 @app.route('/predict', methods=['POST'])
 def predict_scoring():
-    return 'Your predicted score is 0.'
+    prompt_text = request.form['text']
+    return 'prompt_text'
