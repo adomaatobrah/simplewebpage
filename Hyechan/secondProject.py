e6681ad44dcedb69ffea9dd96cbb679d1795b39f
hyechanj <hj48@students.calvin.edu>
2020-06-03 14:39:46 -0400

    removed wildcard from transformers import

--- Hyechan/secondProject.py
+++ Hyechan/secondProject.py
@@ -2,7 +2,7 @@
 #   in PredictWebAppH
 
 import torch
-from transformers import *
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
