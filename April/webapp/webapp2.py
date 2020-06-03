794aa627fb0982a939d843013c85ea60b83cf0df
Kenneth C. Arnold <kenneth.arnold@gmail.com>
2020-06-03 10:49:20 -0400

    Some suggestions for webapp code

--- April/webapp/webapp2.py
+++ April/webapp/webapp2.py
@@ -4,6 +4,10 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 app = Flask(__name__)
 
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+
 @app.route('/')
 def form():
     return render_template("home.html")
@@ -13,11 +17,12 @@ def result():
     prompt_text = request.args['text']
     num_results = int(request.args['number'])
 
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
-
     encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
     prediction_scores, past = model.forward(encoded_prompt)
+    
+    batch_size, num_input_words, vocabulary_size = prediction_scores.shape
+    assert batch_size == 1
+    assert vocabulary_size == 50257
 
     num = 0
     predictability = 0
@@ -29,12 +34,13 @@ def result():
         decoded_prediction_list.append([tokenizer.decode(index.item()) for index in prediction_scores[0, num].topk(num_results).indices])
     
         if encoded_prompt[0, num+1] in prediction_list:
-            predictability = predictability + ((50257 - prediction_list.index(encoded_prompt[0, num+1]))/50257)
+            predictability_rank = prediction_list.index(encoded_prompt[0, num+1])
+            predictability += 1 - predictability_rank / vocabulary_size
         
         num += 1
 
     wordnum = 2
-    for ele in decoded_prediction_list:
+    for wordnum, ele in enumerate(decoded_prediction_list):
         prediction_str = prediction_str + "\nword " + str(wordnum) + ": " + str(ele) 
         wordnum += 1
     
