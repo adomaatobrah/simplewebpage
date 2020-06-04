e394232256d90232265fba496d89342b810ceb83
amv87 <amv87@ds1.cs.calvin.edu>
2020-06-03 21:26:38 -0400

    color coded highlighting for input text

--- April/webapp/webapp2.py
+++ April/webapp/webapp2.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
-
 @app.route('/')
 def form():
     return render_template("home.html")
-
 
 @app.route('/result')
 def result():
@@ -28,6 +26,10 @@ def result():
     num = 0
     predictability = 0
     list_of_dicts = []
+    input_tuples = []
+
+    first_word = tokenizer.decode(encoded_prompt[0, 0].item())
+    input_tuples.append((first_word, "white"))
 
     for x in range(0, encoded_prompt.numel()-1):
         prediction_dict = {}
@@ -44,13 +46,29 @@ def result():
 
         list_of_dicts.append(prediction_dict)
         
+        top10_prediction_list = [(index.item()) for index in prediction_scores[0, num].topk(10).indices]
+        top100_prediction_list = [(index.item()) for index in prediction_scores[0, num].topk(100).indices]
+        
+        next_word = tokenizer.decode(encoded_prompt[0, num+1].item())
+        if encoded_prompt[0, num+1] in top10_prediction_list:
+            input_tuples.append((next_word, "aquamarine"))
+        elif encoded_prompt[0, num+1] in top100_prediction_list:
+            input_tuples.append((next_word, "yellow"))
+        else:
+            input_tuples.append((next_word, "red"))
+
+
+        
         num += 1
 
     final_score = str(predictability/(encoded_prompt.numel()-1))
+
+    print(prediction_scores[0,0].topk(5).indices)
     
     return render_template("home.html", 
                             predictions = list_of_dicts,  
                             usertext = prompt_text, 
-                            score = final_score)
+                            score = final_score,
+                            inputwords = input_tuples)
 
 
