0fb59457cac3317371341bb4e3cb1a8e0484b2b8
hyechanj <hj48@students.calvin.edu>
2020-06-04 15:56:15 -0400

    simplified code and separated into functions

--- Nana/simplewebpage.py
+++ Nana/simplewebpage.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
 app = Flask(__name__)
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
@@ -11,44 +10,56 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 @app.route('/')
 def form():
     return render_template("home.html")
+
 @app.route('/', methods=["GET"])
 def result():
+    prompt_text = request.form["x"]
+    #return prompt_text
+    encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
+    prediction_scores, past = model.forward(encoded_prompt)
 
+    size, words_input, vocab_size = prediction_scores.shape
 
-	prompt_text = request.form["x"]
-	#return prompt_text
-	encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
+    assert vocab_size == 50257
+    element_pos= 0
+    predictability = 0
+    decoded_prediction_list = []
+    tuples = []
+    prediction_str = ""
 
-	num = int(request.form["Num"])
-	prediction_scores, past = model.forward(encoded_prompt)
+    for word in range (0, encoded_prompt.numel()-1):
+        prediction_list = [(index.item()) for index in prediction_scores[0, element_pos].topk(100).indices]
+        decoded_prediction_list=[tokenizer.decode(index.item()) for index in prediction_scores[0, element_pos].topk(100).indices]
+        print(decoded_prediction_list)
 
-	size, words_input, vocab_size = prediction_scores.shape
+    
+        if encoded_prompt[0, element_pos+1] in prediction_list[0:num]:
+            predictability_rank = prediction_list.index(encoded_prompt[0, element_pos+1])
+            predictability += 1 - predictability_rank / vocab_size
 
-	assert vocab_size == 50257
-	element_pos= 0
-	predictability = 0
-	decoded_prediction_list = []
-	prediction_str = ""
+        prediction_list_high = [(index.item()) for index in prediction_scores[0, element_pos].topk(10).indices]
+        decoded_prediction_list_high=[tokenizer.decode(index.item()) for index in prediction_scores[0, element_pos].topk(10).indices]
+        
+        next_word = tokenizer.decode(encoded_prompt[0, element_pos+1].item())
+        if encoded_prompt[0, element_pos+1]  in prediction_list[0:10]:
+            tuples.append((next_word, "green"))
 
-	for word in range (0, encoded_prompt.numel()-1):
-	    prediction_list = [(index.item()) for index in prediction_scores[0, element_pos].topk(num).indices]
-	    decoded_prediction_list=[tokenizer.decode(index.item()) for index in prediction_scores[0, element_pos].topk(num).indices]
-	    
+        elif encoded_prompt[0, element_pos+1] in prediction_list:
+            tuples.append((next_word, "yellow"))
 
-	    if encoded_prompt[0, element_pos+1] in prediction_list:
-	        predictability_rank = prediction_list.index(encoded_prompt[0, element_pos+1])
-	        predictability += 1 - predictability_rank / vocab_size
-	    
-	    	if predictability >=0.8 in decoded_prediction_list:
-	    		High_match = word in decoded_prediction_list
+        else:
+            tuples.append((next_word, "red"))
+            
+        element_pos += 1
 
-	    	elif predictability <0.8>=0.5 in decoded_prediction_list:
-	    		Medium_match = word in decoded_prediction_list
+        
+        print("The predictability score for " + str(num) + " is: " + str(predictability/(encoded_prompt.numel()-1)))
+        return render_template("home.html")
 
 	    	else:
 	    		Low_match = word in decoded_prediction_lists
 
-	    	return decoded_prediction_list
+	    	#return decoded_prediction_list
 
 	    element_pos += 1
 
