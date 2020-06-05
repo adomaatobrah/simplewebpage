1be6aaafd0c0f70bbb377791aaeb5be91e06374b
April Volzer <aprilvolzer@gmail.com>
2020-06-05 11:18:10 -0400

    Nana Ama's web app completed
    Co-authored-by: Kenneth C. Arnold <kenneth.arnold@gmail.com>
    Co-authored-by: adomaatobrah <65881577+adomaatobrah@users.noreply.github.com>
    Co-authored-by: Hyechan Jun <hj48@students.calvin.edu>

--- Nana/simplewebpage.py
+++ Nana/simplewebpage.py
@@ -11,35 +11,47 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 def form():
     return render_template("home.html")
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=["POST"])
 def result():
+    # prompt_text holds the user input
     prompt_text = request.form["x"]
-    #return prompt_text
+    # depth to search
+    num = int(request.form["Num"])
+
+    # encode and generate using the model
     encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
     prediction_scores, past = model.forward(encoded_prompt)
-
+    
+    # variables we need from the model
     size, words_input, vocab_size = prediction_scores.shape
-
     assert vocab_size == 50257
+
+    # index of the word in the phrase
     element_pos= 0
+    # score for each word (additive)
     predictability = 0
+
+    # decoded prediction list used for indexing purposes
     decoded_prediction_list = []
+    # used for color-coding purposes
     tuples = []
     prediction_str = ""
 
-    for word in range (0, encoded_prompt.numel()-1):
+    # for each word in the encoded prompt
+    for word in range(0, encoded_prompt.numel()-1):
+        # create a list of predictions for the next word to that word
         prediction_list = [(index.item()) for index in prediction_scores[0, element_pos].topk(100).indices]
+        # create a decoded version of the same list
         decoded_prediction_list=[tokenizer.decode(index.item()) for index in prediction_scores[0, element_pos].topk(100).indices]
-        print(decoded_prediction_list)
 
-    
+        # if the next word of the encoded prompt is in the user-input range of the prediction_list
         if encoded_prompt[0, element_pos+1] in prediction_list[0:num]:
+            # get the index of the encoded prompt within prediction_list
             predictability_rank = prediction_list.index(encoded_prompt[0, element_pos+1])
+            # score it by dividing by vocab size
             predictability += 1 - predictability_rank / vocab_size
 
-        prediction_list_high = [(index.item()) for index in prediction_scores[0, element_pos].topk(10).indices]
-        decoded_prediction_list_high=[tokenizer.decode(index.item()) for index in prediction_scores[0, element_pos].topk(10).indices]
-        
+    
         next_word = tokenizer.decode(encoded_prompt[0, element_pos+1].item())
         if encoded_prompt[0, element_pos+1]  in prediction_list[0:10]:
             tuples.append((next_word, "green"))
@@ -52,6 +64,12 @@ def result():
             
         element_pos += 1
 
+        # average score for the whole phrase
+        score = str(predictability/(encoded_prompt.numel()-1))
+
+    return render_template("home.html",
+        text_output = tuples,
+        score_output = score)
         
-        print("The predictability score for " + str(num) + " is: " + str(predictability/(encoded_prompt.numel()-1)))
-        return render_template("home.html")
+
+        
