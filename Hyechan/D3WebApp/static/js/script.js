a1509ffe5474c1d1fda3c4e1103f5171be9df945
hyechanj <hj48@students.calvin.edu>
2020-06-08 16:03:22 -0400

    Tried to make tooltips work with d3

--- Hyechan/D3WebApp/static/js/script.js
+++ Hyechan/D3WebApp/static/js/script.js
@@ -1,6 +1,4 @@
 var tooltiptext = d3.select("#1")
-    .style("position", "absolute")
-    .style("visibility", "hidden"); 
 
 d3.select("#tooltip1")
     .on("mouseover", function(){return tooltiptext.style("visibility", "visible");})
