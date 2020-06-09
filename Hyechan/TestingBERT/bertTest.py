66e9f9877ce06d3a1e640f293cf10228eb65f097
hyechanj <hj48@students.calvin.edu>
2020-06-09 13:34:37 -0400

    Began testing with BERT

---
+++ Hyechan/TestingBERT/bertTest.py
@@ -0,0 +1,6 @@
+import torch
+from transformers import BertTokenizer, BertModel
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
